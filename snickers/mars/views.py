acf8c218c245db57fc3c6695e51fdde6c57da404
underlamafile <pavel.filonenko@urfu.me>
2021-06-05 09:52:37 +0500

    Версия 3

--- snickers/mars/views.py
+++ snickers/mars/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 
 # Create your views here.
@@ -26,7 +29,8 @@ def turtle(request):
 
 
 def post_list(request):
-    return render(request, 'mars/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'mars/post_list.html', {'posts': posts})
 
 
 def register(request):
